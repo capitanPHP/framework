9668f46a04c296e0bd76ad28561e75d3abfdfad7
capitanPHP <capitanPHP@tutamail.com>
2025-02-05 10:55:08 +0800

    Update Command.php

--- src/framework/Command.php
+++ src/framework/Command.php
@@ -16,24 +16,28 @@ declare(strict_types=1);
 namespace capitan;
 class Command
 {
-    protected$param = [];
+    protected$param = [
+        '-S'    => '127.0.0.1',
+        '-P'    => 9090
+    ];
+    protected $main = null;
     public function __construct($argv)
     {
+        $this->main = new Main();
         $this->parem($argv);
-        
     }
    
     public function run()
     {
-        echo "Starting PHP built-in web server...". PHP_EOL;
+        echo "Starting PHP built-in web server...\n";
         echo 'Listening on http:\/\/'. $this->param['-S'] .':'. $this->param['-P'] .PHP_EOL;
-        exec('php -S ' .$this->param['-S']. ':' . $this->param['-P']);
+        exec('php -S ' .$this->param['-S']. ':' . $this->param['-P'] . ' -t' . $this->main->getOpenDir());
     }
    
     protected function parem($argv)
     {
        array_shift($argv);
         $chunks =array_chunk($argv, 2);
-        $this->param =array_combine(array_column($chunks, 0), array_column($chunks, 1));
+        $this->param = array_merge($this->param,array_combine(array_column($chunks, 0), array_column($chunks, 1)));
     }
 }
