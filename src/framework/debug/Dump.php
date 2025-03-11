4a9c663b65c1431e69792f66b7cd199d7ba3b3b4
capitanPHP <capitanPHP@tutamail.com>
2025-03-11 13:11:09 +0800

    Fix view style

--- src/framework/debug/Dump.php
+++ src/framework/debug/Dump.php
@@ -1,4 +1,15 @@
 <?php
+/************************************************************************
+* @file Dump.php
+*************************************************************************
+* This file is part of the CapitanPHP framework.
+*************************************************************************
+* Copyright (c) 2025 CapitanPHP.
+*************************************************************************
+* Licensed (https:
+*************************************************************************
+* Author: capitan <capitanPHP@tutamail.com>
+**************************************************************************/
 declare(strict_types=1);
 namespace capitan\debug;
 class Dump
@@ -6,23 +17,41 @@ class Dump
     public static function var($vars)
     {
         ob_start();
-        var_dump(...$vars);
+        var_dump(...$vars[0]);
 
         $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', ob_get_clean());
 
         if (PHP_SAPI == 'cli') return PHP_EOL . $output . PHP_EOL;
 
-        $output = preg_replace_callback('/\[(.*?)\] => (string|array|float|int)\((\d+|\d+.\d+)\) "(.*?)"/', function($matches) {
-            $keyword = '<span class="keyword">' . $matches[1] . '</span>';
-            $type = '<span class="type">' . $matches[2] . '</span>';
-            $int = '<span class="number">' . $matches[3] . '</span>';
-            $value = '<span class="string">' . $matches[4] . '</span>';
+       
+        $output = preg_replace_callback('/\["(.*?)"\]/', function($matches) {
+            $key = '<span class="key">' . $matches[1] . '</span>';
+            return "[\"$key\"]";
+        }, $output);
 
-            return "[$keyword] => $type($int) \"$value\"";
+       
+        $output = preg_replace_callback('/(float|int)\((\d+|\d+.\d+)\)/', function($matches) {
+            $type = '<span class="type">' . $matches[1] . '</span>';
+            $number = '<span class="number">' . $matches[2] . '</span>';
+            return "$type($number)";
+        }, $output);
+
+       
+        $output = preg_replace_callback('/(string)\((\d+|\d+.\d+)\) "(.*?)"/', function($matches) {
+            $type = '<span class="type">' . $matches[1] . '</span>';
+            $number = '<span class="number">' . $matches[2] . '</span>';
+            $value = '<span class="string">' . $matches[3] . '</span>';
+            return "$type($number) \"$value\"";
+        }, $output);
+       
+        $output = preg_replace_callback('/(array)\((\d+|\d+.\d+)\) \{/', function($matches) {
+            $type = '<span class="type">' . $matches[1] . '</span>';
+            $number = '<span class="number">' . $matches[2] . '</span>';
+            return "$type($number) {";
         }, $output);
         
         
-        
-        return '<style>.number{color: red;}.string{color:green;}.type{color:blue;}.keyword{color:purple;}</style>' . '<pre><code>' . $output . '</code></pre>';
+
+        return '<style>body{color:#585260;font-size:16px;background-color:#efecf4}body pre{border:1px dashed #bdbac2;border-style:dashed;padding:1em;border-radius:3px;-moz-border-radius:3px;-webkit-border-radius:3px;-khtml-border-radius:3px;-o-border-radius:3px}body pre>code>span.number{color:#aa573c}body pre>code>span.string{color:#2a9292}body pre>code>span.type{color:#398bc6}body pre>code>span.key{color:#2a9292}</style>' . '<pre><code>' . $output . '</code></pre>';
     }
 }
